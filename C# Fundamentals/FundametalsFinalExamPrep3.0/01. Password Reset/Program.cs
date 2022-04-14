e125992ac3d0103675fdcd4d30f7b24d9d48110a
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:36:12 +0300

    Add files via upload

---
+++ C# Fundamentals/FundametalsFinalExamPrep3.0/01. Password Reset/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _01._Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                string[] commandArs = command.Split(" ");
+                if (commandArs[0] == "TakeOdd")
+                {
+                    string newPassword = string.Empty;
+                    for (int i = 1; i < password.Length; i += 2)
+                    {
+                        newPassword += password[i];
+                    }
+                    password = newPassword;
+                    Console.WriteLine(password);
+                }
+                else if (commandArs[0] == "Cut")
+                {
+                    int index = int.Parse(commandArs[1]);
+                    int lenght = int.Parse(commandArs[2]);
+
+                    password = password.Remove(index, lenght);
+
+                    Console.WriteLine(password);
+                }
+                else if (commandArs[0] == "Substitute")
+                {
+                    string substring = commandArs[1];
+                    string substetute = commandArs[2];
+                    if (password.Contains(substring))
+                    {
+                        password = password.Replace(substring, substetute);
+                        Console.WriteLine(password);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ C# Fundamentals/FundametalsFinalExamPrep3.0/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[@][#]+[A-Z][A-Za-z0-9]{4,}[A-Z][@][#]+";
+            string digitPattern = @"[0-9]";
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string barcodes = Console.ReadLine();
+
+                Match match = Regex.Match(barcodes, pattern);
+
+                if (!match.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                else
+                {
+                    MatchCollection matches = Regex.Matches(match.Value, digitPattern);   
+
+                    Console.Write($"Product group: ");
+
+                    if (matches.Count == 0)
+                    {
+                        Console.WriteLine("00");
+                        continue;
+                    }
+
+                    foreach (Match m in matches)
+                    {
+                        Console.Write(m.Value);
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+            
+        }
+    }
+}
