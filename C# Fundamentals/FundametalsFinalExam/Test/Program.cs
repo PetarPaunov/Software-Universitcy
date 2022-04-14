c1892548f0fb65570e7f15e037c0d2e8772decca
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:41:20 +0300

    Add files via upload

---
+++ C# Fundamentals/FundametalsFinalExam/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        public static object Print { get; private set; }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a > b ? a : b;
+            Console.WriteLine(c);
+        }
+    }
+}
