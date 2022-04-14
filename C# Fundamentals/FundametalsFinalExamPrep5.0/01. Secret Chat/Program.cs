c4f1e87a825f79b6e8139ea6c118ef23956d4831
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:38:29 +0300

    Add files via upload

---
+++ C# Fundamentals/FundametalsFinalExamPrep5.0/01. Secret Chat/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Secret_Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string commands = Console.ReadLine();
+            StringBuilder newInput = new StringBuilder(input);
+            while (commands != "Reveal")
+            {
+                string[] splited = commands.Split(":|:");
+                string command = splited[0];
+
+                if (command == "InsertSpace")
+                {
+                    int index = int.Parse(splited[1]);
+
+                    input = input.Insert(index, " ");
+                    Console.WriteLine(input);
+                }
+                else if (command == "Reverse")
+                {
+                    string substring = splited[1];
+
+                    if (input.Contains(substring))
+                    {
+                        int index = input.IndexOf(substring);
+                        input = input.Remove(index, substring.Length);
+                        string reversedSubstring = string.Empty;
+
+                        for (int i = substring.Length - 1; i >= 0; i--)
+                        {
+                            reversedSubstring += substring[i];
+                        }
+
+                        input = input.Insert(input.Length, reversedSubstring);
+
+                        Console.WriteLine(input);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    
+                }
+                else if (command == "ChangeAll")
+                {
+                    string substring = splited[1];
+                    string replacement = splited[2];
+
+                    input = input.Replace(substring, replacement);
+                    Console.WriteLine(input);
+                }
+
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine($"You have a new text message: {input}");
+        }
+    }
+}
---
+++ C# Fundamentals/FundametalsFinalExamPrep5.0/02. Mirror Words/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Mirror_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(@(?<firstname>[A-Za-z]{3,})@{2}(?<secondname>[A-Za-z]{3,})@)|(#(?<firstname1>[A-Za-z]{3,})#{2}(?<secondname1>[A-Za-z]{3,})#)
+        }
+    }
+}
