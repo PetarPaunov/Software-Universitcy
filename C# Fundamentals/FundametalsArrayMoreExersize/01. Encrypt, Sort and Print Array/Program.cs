274e1762d2dbf436e54779ad72ba59988b1d1b44
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:24:36 +0300

    Add files via upload

---
+++ C# Fundamentals/FundametalsArrayMoreExersize/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            int[] sums = new int[numberOfStrings]; 
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                string input = Console.ReadLine();
+                char curentChar = 
+            }
+        }
+    }
+}
