78485148fcbd59ea3aeae5058dbf8e46404af697
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:23:12 +0300

    Add files via upload

---
+++ C# Fundamentals/FundametalsAssociativeArraysExercise/07. Student Academy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentsGrades = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!studentsGrades.ContainsKey(studentName))
+                {
+                    studentsGrades.Add(studentName, new List<double>());
+                    studentsGrades[studentName].Add(studentGrade);
+                }
+                else
+                {
+                    studentsGrades[studentName].Add(studentGrade);
+                }
+                
+
+            }
+            studentsGrades = studentsGrades.Where(x => x.Value.Average() >= 4.5).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var student in studentsGrades.OrderByDescending(x => x.Value.Average()))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/FundametalsAssociativeArraysExercise/08. Company Users/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" -> ");
+            Dictionary<string, List<string>> company = new Dictionary<string, List<string>>();
+            while (input[0] != "End")
+            {
+                string companyName = input[0];
+                string employId = input[1];
+
+                if (!company.ContainsKey(companyName))
+                {
+                    company.Add(companyName, new List<string>());
+                    company[companyName].Add(employId);
+                }
+                if(!company[companyName].Contains(employId))
+                {
+                    
+                    company[companyName].Add(employId);
+                }
+                input = Console.ReadLine().Split(" -> ");
+            }
+
+            foreach (var emplay in company.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(emplay.Key);
+                Console.Write("-- ");
+                Console.WriteLine(string.Join("\n-- ", emplay.Value));
+            }
+        }
+    }
+}
