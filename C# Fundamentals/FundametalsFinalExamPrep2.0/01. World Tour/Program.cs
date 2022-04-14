e29f5f79575d07b5fe5c56123aa9fb5af29ee6af
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:34:56 +0300

    Add files via upload

---
+++ C# Fundamentals/FundametalsFinalExamPrep2.0/01. World Tour/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _01._World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            while (input != "Travel")
+            {
+                string[] inputInfo = input.Split(":");
+                string command = inputInfo[0];
+
+                if (command == "Add Stop")
+                {
+                    int index = int.Parse(inputInfo[1]);
+                    string town = inputInfo[2];
+
+                    if (index >= 0)
+                    {
+                        stops = stops.Insert(index, town);
+                    }
+                    Console.WriteLine(stops);
+                }
+                else if (command == "Remove Stop")
+                {
+                    int startIndex = int.Parse(inputInfo[1]);
+                    int endIndex = int.Parse(inputInfo[2]);
+
+                    if (startIndex >= 0 && endIndex < stops.Length)
+                    {
+                        int index = endIndex - startIndex;
+                        stops = stops.Remove(startIndex, index + 1);
+                    }
+                    Console.WriteLine(stops);
+                }
+                else if (command == "Switch")
+                {
+                    string oldString = inputInfo[1];
+                    string newString = inputInfo[2];
+
+                    if (stops.Contains(oldString))  
+                    {
+                        stops = stops.Replace(oldString, newString);
+                    }
+                    Console.WriteLine(stops);
+                }
+    
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}
---
+++ C# Fundamentals/FundametalsFinalExamPrep2.0/02. Destination Mapper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([=\/]{1})([A-Z][A-Za-z]{2,})\1{1}";
+            string input = Console.ReadLine();
+            int travelPoint = 0;
+            MatchCollection destination = Regex.Matches(input, pattern);
+            List<string> endpfase = new List<string>();
+            Console.Write("Destinations: ");
+
+
+            foreach (Match match in destination)
+            {
+                
+                string currentDestination = match.Groups[2].Value;
+
+                travelPoint += currentDestination.Length;
+
+                endpfase.Add(currentDestination);  
+            }
+
+            Console.WriteLine(string.Join(", ", endpfase));
+
+            Console.WriteLine($"Travel Points: {travelPoint}");
+
+
+        }
+    }
+}
