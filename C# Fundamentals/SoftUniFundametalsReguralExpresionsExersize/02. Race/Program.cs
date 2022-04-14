18156d879e8da1894d3e5e6109a7ff2ec8ed3c55
PetarPaunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-14 19:17:49 +0300

    Add files via upload

---
+++ C# Fundamentals/SoftUniFundametalsReguralExpresionsExersize/02. Race/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] participents = Console.ReadLine()
+                .Split(", ");
+
+            Dictionary<string, int> racers = new Dictionary<string, int>();
+            string namePatern = @"[A-Za-z]";
+            string digitPatern = @"[0-9]";
+
+            foreach (var item in participents)
+            {
+                racers.Add(item, 0);
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "end of race")  
+            {
+                StringBuilder name = new StringBuilder();
+
+                int kilometars = 0;
+
+                MatchCollection letters = Regex.Matches(input, namePatern);
+
+                MatchCollection digits = Regex.Matches(input, digitPatern);
+                 
+                foreach (Match letter in letters)
+                {
+                    name.Append(letter);
+                }
+
+                foreach (Match digt in digits)
+                {
+                    kilometars += int.Parse(digt.Value);
+                }
+
+                if (!racers.ContainsKey(name.ToString()))
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    racers[name.ToString()] += kilometars;
+                }
+
+                input = Console.ReadLine();
+            }
+            int counter = 1;
+            foreach (var racer in racers.OrderByDescending(x => x.Value))
+            {
+                
+                if (counter == 1)
+                {
+                    Console.WriteLine($"1st place: {racer.Key}");
+                    
+                }
+                else if (counter == 2)
+                {
+                    Console.WriteLine($"2nd place: {racer.Key}");
+                }
+                else if (counter == 3)
+                {
+                    Console.WriteLine($"3rd place: {racer.Key}");
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/SoftUniFundametalsReguralExpresionsExersize/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace _03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^%([A-Z]{1}[a-z]+)%.*<(\w+)>.*\|([0-9]+)\|.*?([0-9.]+)\$$";
+
+            string input = Console.ReadLine();
+
+            decimal totalSum = 0;
+
+            while (input != "end of shift")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if (!match.Success)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                string name = match.Groups[1].Value;
+                string product = match.Groups[2].Value;
+                int qty = int.Parse(match.Groups[3].Value);
+                decimal price = decimal.Parse(match.Groups[4].Value);
+
+                decimal totalPrice = qty * price;
+
+                
+                totalSum += totalPrice;
+
+                Console.WriteLine($"{name}: {product} - {totalPrice:F2}");
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {totalSum:F2}");
+        }
+    }
+}
