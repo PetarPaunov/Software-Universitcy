dc998dcadb8ede764812946948768b0c9aedf5b8
P.Paunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-15 13:39:13 +0300

    Add files via upload

---
+++ C# Advanced/FuncitonalProgrammingExersize/03. Custom Min Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minNumber = numbers =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var number in numbers)
+                {
+                    if (minValue > number)
+                    {
+                        minValue = number;
+                    }
+                }
+                return minValue;
+            };
+
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(minNumber(input));
+        }
+    }
+}
---
+++ C# Advanced/FuncitonalProgrammingExersize/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int startNum = input[0];
+            int endNum = input[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> even = number => number % 2 == 0;
+
+            string command = Console.ReadLine();
+
+            if (command == "even")
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => even(x))));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => !even(x))));
+            }
+            
+        }
+    }
+}
