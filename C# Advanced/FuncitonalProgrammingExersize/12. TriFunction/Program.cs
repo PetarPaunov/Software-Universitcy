163da5c6344bb2c95241690148ce23e42b005bdb
P.Paunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-15 13:44:05 +0300

    Add files via upload

---
+++ C# Advanced/FuncitonalProgrammingExersize/12. TriFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split().ToArray();
+
+            Func<string, int, bool> isEqualToMagigNum = (name, sum)
+                => name.Sum(x => x) >= sum;
+
+            Func<string[], int, Func<string, int, bool>, string> finalResult
+                = (allNames, number, func)
+                => allNames.FirstOrDefault(x => func(x, number));
+
+            string magicName = finalResult(names, magicNumber, isEqualToMagigNum);
+
+            Console.WriteLine(magicName);
+        }
+    }
+}
---
+++ C# Advanced/FuncitonalProgrammingExersize/FuncitonalProgrammingExersize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SoftUniAdancedFuncitonalProgrammingExersize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            Action<string> names = name => Console.WriteLine(name);
+
+
+            foreach (var name in input)
+            {
+                names(name);
+            }
+
+
+        }
+    }
+}
