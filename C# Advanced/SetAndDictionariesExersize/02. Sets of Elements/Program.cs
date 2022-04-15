f5303219b827e0f30b6e712a7bc4e72a579aa4c1
P.Paunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-15 14:00:57 +0300

    Add files via upload

---
+++ C# Advanced/SetAndDictionariesExersize/02. Sets of Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int firstSetNums = input[0];
+            int secondSetNums = input[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < firstSetNums; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                firstSet.Add(num);
+            }
+            for (int i = 0; i < secondSetNums; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondSet.Add(num);
+            }
+
+            List<int> numbers = firstSet.Intersect(secondSet).ToList();
+
+            foreach (var num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
---
+++ C# Advanced/SetAndDictionariesExersize/03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> sortedChemicals = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemicals = Console.ReadLine().Split(" ");
+
+                for (int j = 0; j < chemicals.Length; j++)
+                {
+                    sortedChemicals.Add(chemicals[j]);
+                }
+            }
+
+            foreach (var chmical in sortedChemicals)
+            {
+                Console.Write(chmical + " ");
+            }
+        }
+    }
+}
