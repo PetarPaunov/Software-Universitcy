b2da4ce568b9ffc447d74f8faeccc435fbb09aba
P.Paunov <85368212+PetarPaunov@users.noreply.github.com>
2022-04-15 13:48:34 +0300

    Add files via upload

---
+++ C# Advanced/MultidaimationalArrayExersize/2. Squares in Matrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fisrtInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int rows = fisrtInput[0];
+            int cols = fisrtInput[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int couter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col +1] && 
+                        matrix[row, col] == matrix[row + 1, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col])
+                    {
+                        couter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(couter);
+        }
+    }
+}
---
+++ C# Advanced/MultidaimationalArrayExersize/3. Maximal Sum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int currentrow = 0;
+            int curretcol = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int squereSum = 0;
+                    squereSum += matrix[row, col] + matrix[row, col + 1] +
+                                 matrix[row, col + 2] + matrix[row + 1, col] +
+                                 matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                                 matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (maxSum < squereSum)
+                    {
+                        maxSum = squereSum;
+                        currentrow = row;
+                        curretcol = col;
+                    }
+                }
+
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = currentrow; row <= currentrow + 2; row++)
+            {
+                for (int col = curretcol; col <= curretcol + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
