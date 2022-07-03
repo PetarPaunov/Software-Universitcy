b3294af0999027b7c9722843bac73669781f84ce
PetarPaunov <petarp.2000@abv.bg>
2022-07-03 17:29:11 +0300

    Add problem P01_StudentSystem

---
+++ Entity Framework Core/Entity Relations/P01_StudentSystem/Migrations/20220702160226_fixes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Migrations
+{
+    public partial class fixes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Students",
+                type: "char(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "CHAR(10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Students",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Resources",
+                type: "varchar(2048)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(MAX)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "HomeworkSubmissions",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(MAX)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Students",
+                type: "CHAR(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "char(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Resources",
+                type: "VARCHAR(MAX)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(2048)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "HomeworkSubmissions",
+                type: "VARCHAR(MAX)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+        }
+    }
+}
