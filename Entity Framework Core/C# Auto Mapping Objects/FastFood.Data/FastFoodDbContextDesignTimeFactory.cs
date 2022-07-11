aa0943e732577bb6ca34fdd1a768676c5a01aa9e
PetarPaunov <petarp.2000@abv.bg>
2022-07-11 22:46:51 +0300

    C# Auto Mapping Objects Practis

---
+++ Entity Framework Core/C# Auto Mapping Objects/FastFood.Data/FastFoodDbContextDesignTimeFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace FastFood.Data
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FastFoodDbContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=DESKTOP-VFG6D1G\\SQLEXPRESS;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
