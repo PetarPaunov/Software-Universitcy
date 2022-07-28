332fa4bca6c02fe76606d58ef0005fbbc40b309c
PetarPaunov <petarp.2000@abv.bg>
2022-07-28 12:53:22 +0300

    C# DB Advanced Exam - 08 August 2020 100% working

---
+++ Entity Framework Core/Exam Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Dto/Export/ExportUsersModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.Dto.Export
+{
+    [XmlType("User")]
+    public class ExportUsersModel
+    {
+        [XmlAttribute("username")]
+        public string Username { get; set; }
+
+        [XmlArray("Purchases")]
+        public PurchesesExportModel[] Purchases { get; set; }
+
+        [XmlElement("TotalSpent")]
+        public decimal TotalSpent { get; set; }
+    }
+
+    [XmlType("Purchase")]
+    public class PurchesesExportModel
+    {
+        [XmlElement("Card")]
+        public string Card { get; set; }
+
+        [XmlElement("Cvc")]
+        public string Cvs { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+        [XmlElement("Game")]
+        public GameExportModel Game { get; set; }
+    }
+
+    public class GameExportModel
+    {
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        [XmlElement("Genre")]
+        public string Genre { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+    }
+}
+
+//< Users >
+//  < User username = "mgraveson" >
+//    < Purchases >
+//      < Purchase >
+//        < Card > 7991 7779 5123 9211 </ Card >
+//        < Cvc > 340 </ Cvc >
+//        < Date > 2017 - 08 - 31 17:09 </ Date >
+//        < Game title = "Counter-Strike: Global Offensive" >
+//          < Genre > Action </ Genre >
+//          < Price > 12.49 </ Price >
+//        </ Game >
+//      </ Purchase >
+
--- Entity Framework Core/Exam Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exam Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
@@ -1,18 +1,86 @@
 ﻿namespace VaporStore.DataProcessor
 {
 	using System;
-	using Data;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using VaporStore.DataProcessor.Dto.Export;
 
-	public static class Serializer
+    public static class Serializer
 	{
 		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
 		{
-			throw new NotImplementedException();
+			var gameByGenres = context.Genres.ToList()
+				.Where(x => genreNames.Contains(x.Name))
+				.Select(x => new
+				{
+					Id = x.Id,
+					Genre = x.Name,
+					Games = x.Games.Select(g => new
+					{
+						Id = g.Id,
+						Title = g.Name,
+						Developer = g.Developer.Name,
+						Tags = string.Join(", ", g.GameTags.Select(gt => gt.Tag.Name)),
+						Players = g.Purchases.Count()
+					})
+					.Where(x => x.Players > 0)
+					.OrderByDescending(x => x.Players)
+					.ThenBy(x => x.Id),
+					TotalPlayers = x.Games.Sum(x => x.Purchases.Count())
+				})
+				.OrderByDescending(x => x.TotalPlayers)
+				.ThenBy(x => x.Id);
+
+
+			var result = JsonConvert.SerializeObject(gameByGenres, Newtonsoft.Json.Formatting.Indented);
+
+			return result.ToString();
 		}
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
 		{
-			throw new NotImplementedException();
+			var users = context.Users
+				.ToList()
+				.Where(x => x.Cards.Any(x => x.Purchases.Any(p => p.Type.ToString() == storeType)))
+				.Select(x => new ExportUsersModel
+				{
+					Username = x.Username,
+					Purchases = x.Cards.SelectMany(c => c.Purchases)
+					    .Where(p => p.Type.ToString() == storeType)
+						.Select(p => new PurchesesExportModel
+						{
+							Card = p.Card.Number,
+							Cvs = p.Card.Cvc,
+							Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+							Game = new GameExportModel
+							{
+								Title = p.Game.Name,
+								Genre = p.Game.Genre.Name,
+								Price = p.Game.Price
+							}
+						})
+						.OrderBy(x => x.Date)
+						.ToArray(),
+					TotalSpent = x.Cards
+					.Sum(p => p.Purchases.Where(p => p.Type.ToString() == storeType)
+					.Sum(p => p.Game.Price))
+				})
+				.OrderByDescending(x => x.TotalSpent)
+				.ThenBy(x => x.Username)
+				.ToArray();
+
+			var xmlSerializer = new XmlSerializer(typeof(ExportUsersModel[]), new XmlRootAttribute("Users"));
+
+			var stringWriter = new StringWriter();
+			var ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+			xmlSerializer.Serialize(stringWriter, users, ns);
+
+			return stringWriter.ToString();
 		}
 	}
 }
