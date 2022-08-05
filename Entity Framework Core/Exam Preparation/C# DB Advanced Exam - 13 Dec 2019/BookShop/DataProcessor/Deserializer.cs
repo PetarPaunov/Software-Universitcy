e41166ae2792ec71300de933550ac3d00bdbede8
PetarPaunov <petarp.2000@abv.bg>
2022-08-05 22:58:10 +0300

    C# DB Advanced Exam - 13 Dec 2019 100% working

---
+++ Entity Framework Core/Exam Preparation/C# DB Advanced Exam - 13 Dec 2019/BookShop/DataProcessor/Deserializer.cs
@@ -0,0 +1,159 @@
+﻿namespace BookShop.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using BookShop.Data.Models;
+    using BookShop.Data.Models.Enums;
+    using BookShop.DataProcessor.ImportDto;
+    using Data;
+    using Newtonsoft.Json;
+    using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedBook
+            = "Successfully imported book {0} for {1:F2}.";
+
+        private const string SuccessfullyImportedAuthor
+            = "Successfully imported author - {0} with {1} books.";
+
+        public static string ImportBooks(BookShopContext context, string xmlString)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportBooksModel[]), new XmlRootAttribute("Books"));
+
+            var stringReader = new StringReader(xmlString);
+
+            var bookDto = xmlSerializer.Deserialize(stringReader) as ImportBooksModel[];
+
+            var sb = new StringBuilder();
+
+            foreach (var book in bookDto)
+            {
+                if (!IsValid(book))
+                {
+                    sb.AppendLine("Invalid data!");
+                    continue;
+                }
+
+                if (!Enum.IsDefined(typeof(Genre), book.Genre))
+                {
+                    sb.AppendLine("Invalid data!");
+                    continue;
+                }
+
+                var bookDate = DateTime
+                    .TryParseExact(
+                        book.PublishedOn, 
+                        "MM/dd/yyyy", 
+                        CultureInfo.InvariantCulture, 
+                        DateTimeStyles.None, 
+                        out var currentDate
+                    );
+
+                if (!bookDate)
+                {
+                    sb.AppendLine("Invalid data!");
+                    continue;
+                }
+
+                var currentGenre = Enum.Parse<Genre>(Enum.GetName(typeof(Genre), book.Genre));
+
+
+                var currentBook = new Book
+                {
+                    Name = book.Name,
+                    Genre = currentGenre,
+                    Price = book.Price,
+                    Pages = book.Pages,
+                    PublishedOn = currentDate,
+
+                };
+
+                context.Books.Add(currentBook);
+                context.SaveChanges();
+
+                sb.AppendLine($"Successfully imported book {currentBook.Name} for {currentBook.Price:F2}.");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportAuthors(BookShopContext context, string jsonString)
+        {
+            var authorsDto = JsonConvert.DeserializeObject<ImportAuthorsModel[]>(jsonString);
+
+            var sb = new StringBuilder();
+
+            foreach (var author in authorsDto)
+            {
+                if (!IsValid(author))
+                {
+                    sb.AppendLine("Invalid data!");
+                    continue;
+                }
+
+                var emailExists = context.Authors.Any(x => x.Email == author.Email);
+
+                if (emailExists)
+                {
+                    sb.AppendLine("Invalid data!");
+                    continue;
+                }
+
+                var currentAuthor = new Author
+                {
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                    Phone = author.Phone,
+                    Email = author.Email
+                };
+
+                foreach (var book in author.Books)
+                {
+                    var bookIsNull = context.Books.Find(book.Id);
+
+                    if (bookIsNull == null)
+                    {
+                        continue;
+                    }
+
+                    var currentBook = new AuthorBook
+                    {
+                        Book = bookIsNull,
+                        Author = currentAuthor
+                    };
+
+                    currentAuthor.AuthorsBooks.Add(currentBook);
+                }
+
+                if (currentAuthor.AuthorsBooks.Count() == 0)
+                {
+                    sb.AppendLine("Invalid data!");
+                    continue;
+                }
+
+                context.Authors.Add(currentAuthor);
+                context.SaveChanges();
+
+                sb.AppendLine($"Successfully imported author - {currentAuthor.FirstName + ' ' + currentAuthor.LastName} with {currentAuthor.AuthorsBooks.Count()} books.");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Entity Framework Core/Exam Preparation/C# DB Advanced Exam - 13 Dec 2019/BookShop/DataProcessor/Serializer.cs
@@ -0,0 +1,70 @@
+﻿namespace BookShop.DataProcessor
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using BookShop.DataProcessor.ExportDto;
+    using Data;
+    using Newtonsoft.Json;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportMostCraziestAuthors(BookShopContext context)
+        {
+            var authors = context
+                .Authors
+                .Select(x => new
+                {
+                    AuthorName = x.FirstName + " " + x.LastName,
+                    Books = x.AuthorsBooks
+                        .Select(ab => ab.Book)
+                        .OrderByDescending(b => b.Price)
+                        .Select(b => new
+                        {
+                            BookName = b.Name,
+                            BookPrice = b.Price.ToString("F2")
+                        })
+                        .ToList()
+                })
+                .ToList()
+                .OrderByDescending(x => x.Books.Count())
+                .ThenBy(x => x.AuthorName)
+                .ToList();
+
+            return JsonConvert.SerializeObject(authors, Formatting.Indented);
+        }
+
+        public static string ExportOldestBooks(BookShopContext context, DateTime date)
+        {
+            var oldestBooks = context
+                .Books
+                .Where(x => x.PublishedOn < date && x.Genre.ToString() == "Science")
+                .OrderByDescending(x => x.Pages)
+                .ThenByDescending(x => x.PublishedOn)
+                .Select(x => new ExportOldestBooksModel
+                {
+                    Pages = x.Pages.ToString(),
+                    Name = x.Name,
+                    Date = x.PublishedOn.ToString("d", CultureInfo.InvariantCulture)
+                })
+                .ToArray()
+                .Take(10)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportOldestBooksModel[]), new XmlRootAttribute("Books"));
+
+            var textWriter = new StringWriter();
+
+            var ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            xmlSerializer.Serialize(textWriter, oldestBooks, ns);
+
+            return textWriter.ToString();
+        }
+    }
+}
