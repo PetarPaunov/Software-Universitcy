ad86859543e4312dd710f8571bba64a1f08c26d5
PetarPaunov <petarp.2000@abv.bg>
2022-10-13 20:39:53 +0300

    New configurations added

--- ASP.NET/TaskBoardApp/TaskBoardApp.Core/Data/ApplicationDbContext.cs
+++ ASP.NET/TaskBoardApp/TaskBoardApp.Core/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace TaskBoardApp.Data
+namespace TaskBoardApp.Core.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
