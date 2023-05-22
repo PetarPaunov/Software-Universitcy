b8988e9bf41d0198f6ee9dd06055315d8c14800b
PetarPaunov <petarp.2000@abv.bg>
2023-05-22 22:10:01 +0300

    Firs two prblems done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Backtracking/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Recursive_Array_Sum
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(GetSum(arr, 0));
+        }
+
+        private static int GetSum(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + GetSum(arr, index + 1);
+        }
+    }
+}
---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Backtracking/02. Recursive Drawing/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02._Recursive_Drawing
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            RecursiveDrawing(n);
+        }
+
+        private static void RecursiveDrawing(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            RecursiveDrawing(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+    }
+}
