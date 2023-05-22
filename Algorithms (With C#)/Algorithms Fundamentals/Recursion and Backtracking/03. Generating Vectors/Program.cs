709f3b3753d94d483398f9f1d7729d8c43561d94
PetarPaunov <petarp.2000@abv.bg>
2023-05-22 22:26:36 +0300

    Thrird problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Backtracking/03. Generating Vectors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03._Generating_Vectors
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var arr = new int[n];
+
+            GeneratingVectors(arr, 0);
+        }
+
+        private static void GeneratingVectors(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[index] = i;
+                GeneratingVectors(arr, index + 1);
+            }
+        }
+    }
+}
