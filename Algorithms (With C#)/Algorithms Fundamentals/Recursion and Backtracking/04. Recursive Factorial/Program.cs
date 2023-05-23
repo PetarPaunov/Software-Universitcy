15026c212934a51aa22cea9b77279c5ff19b0cbf
PetarPaunov <petarp.2000@abv.bg>
2023-05-23 10:38:45 +0300

    Forth problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Backtracking/04. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._Recursive_Factorial
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorialRecursivly(n));
+        }
+
+        private static int GetFactorialRecursivly(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * GetFactorialRecursivly(n - 1);
+        }
+    }
+}
