cc5fff8d46bd1244d4cf6744da7f9367d4706568
PetarPaunov <petarp.2000@abv.bg>
2023-05-25 22:22:28 +0300

    Last problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Backtracking/07. Recursive Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07._Recursive_Fibonacci
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var fib = RecursiveFibonacci(n);
+
+            Console.WriteLine(fib);
+        }
+
+        private static long RecursiveFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+        }
+    }
+}
