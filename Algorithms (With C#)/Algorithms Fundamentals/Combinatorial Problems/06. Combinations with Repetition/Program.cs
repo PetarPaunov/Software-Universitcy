c7d95be683d30813a194ac0bd6929e037e9f0c62
PetarPaunov <petarp.2000@abv.bg>
2023-05-27 23:29:29 +0300

    Sixth problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Combinatorial Problems/06. Combinations with Repetition/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06._Combinations_with_Repetition
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static string[] input;
+        private static int n;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split().ToArray();
+            n = int.Parse(Console.ReadLine());
+
+            combinations = new string[n];
+
+            GenerateCombinations(0, 0);
+        }
+
+        private static void GenerateCombinations(int index, int startIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = startIndex; i < input.Length; i++)
+            {
+                combinations[index] = input[i];
+                GenerateCombinations(index + 1, i);
+            }
+        }
+    }
+}
