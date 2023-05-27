b31c92ccfa7e20271d0ed204641258ebbd7f139b
PetarPaunov <petarp.2000@abv.bg>
2023-05-27 23:39:15 +0300

    Last problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Combinatorial Problems/07. N Choose K Count/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07._N_Choose_K_Count
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(row, col));
+        }
+
+        private static int GetBinom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            return GetBinom(row - 1, col) + GetBinom(row - 1, col - 1);
+        }
+    }
+}
