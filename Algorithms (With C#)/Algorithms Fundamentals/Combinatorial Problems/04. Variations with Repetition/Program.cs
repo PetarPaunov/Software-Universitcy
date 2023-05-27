27f4ece2453ab99aa6d3a9f9fc07d65a72b488c0
PetarPaunov <petarp.2000@abv.bg>
2023-05-27 23:11:51 +0300

    Forth problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Combinatorial Problems/04. Variations with Repetition/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04._Variations_with_Repetition
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static string[] input;
+        private static int n;
+        private static string[] variations;
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split().ToArray();
+            n = int.Parse(Console.ReadLine());
+
+            variations = new string[n];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(String.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                variations[index] = input[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
