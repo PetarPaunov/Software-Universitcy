dd47e52f52a81ac0996ed7c8c895b8c96507e14b
PetarPaunov <petarp.2000@abv.bg>
2023-06-01 23:08:16 +0300

    First problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Combinatorial Problems - Exercise/1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static int[] inputArr;
+        static void Main(string[] args)
+        {
+            inputArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            ReversArray(0);
+        }
+
+        private static void ReversArray(int index)
+        {
+            if (index >= inputArr.Length - 1 - index)
+            {
+                Console.WriteLine(String.Join(" ", inputArr));
+                return;
+            }
+
+            var temp = inputArr[index];
+            inputArr[index] = inputArr[inputArr.Length - 1 - index];
+            inputArr[inputArr.Length - 1 - index] = temp;
+
+            ReversArray(index + 1);
+        }
+    }
+}
