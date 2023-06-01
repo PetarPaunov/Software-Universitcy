ff6be76b37814e09e9e952b4c122e9f786e2fff4
PetarPaunov <petarp.2000@abv.bg>
2023-06-01 23:25:23 +0300

    Second problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Recursion and Combinatorial Problems - Exercise/2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2
+{
+    using System;
+
+    internal class Program
+    {
+        private static int[] elements;
+        
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            elements = new int[n];
+
+            SimulateNestedLoop(0);
+        }
+
+        private static void SimulateNestedLoop(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(String.Join(' ', elements));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[index] = i + 1;
+
+                SimulateNestedLoop(index + 1);
+            }
+        }
+    }
+}
