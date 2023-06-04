cd2cfaa0eb119f46e969e3e4dad6feda6f8a82ee
PetarPaunov <petarp.2000@abv.bg>
2023-06-04 18:05:31 +0300

    Third problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms/Bubble Sort/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Bubble_Sort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            BubbleSort(numbers);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            var sortCount = 0;
+            var isSorted = false;
+
+            while (!isSorted)
+            {
+                isSorted = true;
+
+                for (int i = 1; i < numbers.Length - sortCount; i++)
+                {
+                    var j = i - 1;
+
+                    if (numbers[j] > numbers[i])
+                    {
+                        Swap(numbers, i, j);
+                        isSorted = false;
+                    }
+                }
+
+                sortCount++;
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
