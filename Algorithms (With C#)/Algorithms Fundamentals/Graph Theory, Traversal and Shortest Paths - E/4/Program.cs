0131ada996faa5320d78810cf099e54f08f369b9
PetarPaunov <petarp.2000@abv.bg>
2023-06-11 17:20:57 +0300

    Forth problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Graph Theory, Traversal and Shortest Paths - E/4/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _4
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static Dictionary<int, int> visited;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[n];
+            visited = new Dictionary<int, int>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<int>();
+
+                var children = Console.ReadLine();
+
+                for (int child = 0; child < children.Length; child++)
+                {
+                    if (children[child] == 'Y')
+                    {
+                        graph[node].Add(child);
+                    }
+                }
+            }
+
+            var result = 0;
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                result += DFS(node);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int DFS(int node)
+        {
+            if (visited.ContainsKey(node))
+            {
+                return visited[node]; 
+            }
+
+            var salary = 0;
+
+            if (graph[node].Count == 0)
+            {
+                salary = 1;
+            }
+            else
+            {
+                foreach (var child in graph[node])
+                {
+                    salary += DFS(child);
+                }
+            }
+
+            visited[node] = salary;
+
+            return salary;
+        }
+    }
+}
