350a247b9c52e52c0eaf665523e19dde406dc07b
PetarPaunov <petarp.2000@abv.bg>
2023-06-12 15:08:57 +0300

    First problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Introduction to Dynamic Programming/01. Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01._Fibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static Dictionary<int, long> fibPairs = new Dictionary<int, long>();
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibubacci(n));
+        }
+
+        private static long Fibubacci(int n)
+        {
+            if (fibPairs.ContainsKey(n))
+            {
+                return fibPairs[n];
+            }
+
+            if (n < 2)
+            {
+                return n;
+            }
+
+            var result = Fibubacci(n - 1) + Fibubacci(n - 2);
+            fibPairs[n] = result;
+
+            return result;
+        }
+    }
+}
