7426d115c98315c5ebcafed398f764fb36fff709
PetarPaunov <petarp.2000@abv.bg>
2023-06-20 13:32:19 +0300

    Second task done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Exam - 03 Jan 2021/02. Socks/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02._Socks
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static int[,] dp;
+        static void Main(string[] args)
+        {
+            var firstLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var secondLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            dp = new int[firstLine.Length + 1, secondLine.Length + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    if (firstLine[row - 1] == secondLine[col - 1])
+                    {
+                        dp[row, col] = dp[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[row, col] = Math.Max(dp[row - 1, col], dp[row, col - 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine(dp[firstLine.Length, secondLine.Length]);
+        }
+    }
+}
