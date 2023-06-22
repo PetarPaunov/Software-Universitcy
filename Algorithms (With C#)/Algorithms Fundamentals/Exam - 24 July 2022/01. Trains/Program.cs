2ffe069b9010ea3782cf249d1745369472c0947d
PetarPaunov <petarp.2000@abv.bg>
2023-06-22 23:04:35 +0300

    First problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Exam - 24 July 2022/01. Trains/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _01._Trains
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrival = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .OrderBy(x => x)
+                .ToArray();
+
+            var departure = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(FindAllRequiredPlatforms(arrival, departure));
+        }
+
+        private static int FindAllRequiredPlatforms(double[] arrival, double[] departure)
+        {
+            var arrivalPointer = 0;
+            var departurePointer = 0;
+
+            var requiredPlatforms = 0;
+            var platforms = 0;
+
+            while (arrivalPointer < arrival.Length)
+            {
+                var arrivalTrain = arrival[arrivalPointer];
+                var departureTrain = departure[departurePointer];
+
+                if (arrivalTrain < departureTrain)
+                {
+                    platforms++;
+                    arrivalPointer++;
+                    requiredPlatforms = Math.Max(platforms, requiredPlatforms);
+                }
+                else
+                {
+                    departurePointer++;
+                    platforms--;
+                }
+            }
+
+            return requiredPlatforms;
+        }
+    }
+}
