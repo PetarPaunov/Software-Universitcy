7d1e92fd62d64968bc537fc79a9f5b97ecbc526e
PetarPaunov <petarp.2000@abv.bg>
2023-06-28 21:45:19 +0300

    Second task done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Exam - 24 July 2022/02. Conditional Expression Resolver/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _02._Conditional_Expression_Resolver
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var expression = Console.ReadLine()
+                .Split(' ')
+                .Select(x => x[0])
+                .ToArray();
+
+            Console.WriteLine(ParseExpression(expression, 0));
+        }
+
+        private static int ParseExpression(char[] expression, int index)
+        {
+            if (char.IsDigit(expression[index]))
+            {
+                return expression[index] - '0';
+            }
+
+            if (expression[index] == 't')
+            {
+                return ParseExpression(expression, index + 2);
+            }
+
+            int foundConditions = 0;
+
+            for (int i = index + 2; i < expression.Length; i++)
+            {
+                var currentSymbol = expression[i];
+
+                if (currentSymbol == '?')
+                {
+                    foundConditions++;
+                }
+                else if (currentSymbol == ':')
+                {
+                    foundConditions--;
+
+                    if (foundConditions < 0)
+                    {
+                        return ParseExpression(expression, i + 1);
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
