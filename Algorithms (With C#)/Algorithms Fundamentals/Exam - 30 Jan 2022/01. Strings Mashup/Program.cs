14b6abb6b4d3f3a11c37bbfcb732c21f32cf9faa
PetarPaunov <petarp.2000@abv.bg>
2023-06-28 22:47:46 +0300

    First task done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Exam - 30 Jan 2022/01. Strings Mashup/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01._Strings_Mashup
+{
+    using System;
+
+    internal class Program
+    {
+        private static char[] combinations;
+        private static char[] str;
+
+        static void Main(string[] args)
+        {
+            str = Console.ReadLine().ToCharArray();
+            var n = int.Parse(Console.ReadLine());
+
+            Array.Sort(str);
+
+            combinations = new char[n];
+
+            GenerateCombinations(0, 0);
+        }
+
+        private static void GenerateCombinations(int index, int startIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(String.Join("", combinations));
+                return;
+            }
+
+            for (int i = startIndex; i < str.Length; i++)
+            {
+                combinations[index] = str[i];
+                GenerateCombinations(index + 1, i);
+            }
+        }
+    }
+}
