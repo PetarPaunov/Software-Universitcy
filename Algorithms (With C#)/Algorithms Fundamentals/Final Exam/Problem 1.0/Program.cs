f4bcc3d64272d941a143f7fb13e19d8c618d6b96
PetarPaunov <petarp.2000@abv.bg>
2023-07-01 10:23:25 +0300

    First problem done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Final Exam/Problem 1.0/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem_1._0
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static Dictionary<string, ulong> cache;
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            cache = new Dictionary<string, ulong>();
+
+            Console.WriteLine(GetBinom(n, k));
+        }
+
+        private static ulong GetBinom(int row, int col)
+        {
+            if (col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            var key = $"{row}-{col}";
+
+            if (cache.ContainsKey(key))
+            {
+                return cache[key];
+            }
+
+            var result = GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+
+            cache[key] = result;
+
+            return result;
+
+        }
+    }
+}
