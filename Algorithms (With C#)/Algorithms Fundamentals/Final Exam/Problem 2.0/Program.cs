7312783552a33a48f079d7beb013bb033c752b86
PetarPaunov <petarp.2000@abv.bg>
2023-07-01 11:28:49 +0300

    Second task done

---
+++ Algorithms (With C#)/Algorithms Fundamentals/Final Exam/Problem 2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Problem_2._0
+{
+    using System;
+    using System.Linq;
+    using System.Runtime.Intrinsics.Arm;
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    //Тук идеята е да проследите първа и втора буква
+    //и да видите колко вериги(групи) се получават и да върнете броя.
+    //В първия пример имаме първа група ABCD и FGH.
+}
