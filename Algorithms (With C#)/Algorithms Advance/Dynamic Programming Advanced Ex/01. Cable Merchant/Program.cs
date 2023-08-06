860b0ccaff56c54eb0f30b61a1e111a531571a3c
Petar Paunov <petarp.2000@abv.bg>
2023-08-06 21:07:44 +0300

    First task done.

---
+++ Algorithms (With C#)/Algorithms Advance/Dynamic Programming Advanced Ex/01. Cable Merchant/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _01._Cable_Merchant
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static List<int> prices;
+        private static int[] memo;
+
+        static void Main(string[] args)
+        {
+            prices = new List<int>() { 0 };
+
+            prices.AddRange(Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray());
+
+            var connectorPrice = int.Parse(Console.ReadLine());
+
+            memo = new int[prices.Count];
+
+            CutRob(prices.Count - 1, connectorPrice);
+        }
+
+        private static int CutRob(int length, int connectorPrice)
+        {
+            if (length == 0)
+            {
+                return 0;
+            }
+
+            if (memo[length] != 0)
+            {
+                return memo[length];
+            }
+
+            var currentBestPrice = prices[length];
+
+            for (int i = 1; i < length; i++)
+            {
+                var currentPrice = prices[i] + CutRob(length - i, connectorPrice) - 2 * connectorPrice;
+
+                if (currentPrice > currentBestPrice)
+                {
+                    currentBestPrice = currentPrice;
+                }
+            }
+
+            memo[length] = currentBestPrice;
+
+            return currentBestPrice;
+        }
+    }
+}
